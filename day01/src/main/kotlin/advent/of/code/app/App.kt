758d3e6a2efa4fd4fa14ffe430c880cc8fb235a0
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2022-12-01 23:01:17 +0100

    Day 1 solution

--- day01/src/main/kotlin/advent/of/code/app/App.kt
+++ day01/src/main/kotlin/advent/of/code/app/App.kt
@@ -1,6 +1,23 @@
 package advent.of.code.app
 
+import java.io.File;
+
 
 fun main() {
-    println(MessageUtils.getMessage())
+    var sums = mutableListOf<Int>();
+    var sum = 0;
+
+    File("day01-part1.txt").forEachLine {
+        if (it == "") {
+            sums.add(sum);
+            sum = 0;
+        }
+        else {
+            sum += it.toInt();
+        }
+    }
+
+    val sorted_sums = sums.sortedDescending();
+    println("${sorted_sums[0]}");
+    println("${sorted_sums[0] + sorted_sums[1] + sorted_sums[2]}");
 }
